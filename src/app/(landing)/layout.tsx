0f8c19eda62ae951649df21a85d074d9ca772fd5
Abdul Rafay Shaikh <abdul.rafay@mayabytes.com>
2024-05-09 03:42:13 +0500

    chore: Refactor landing page layout and remove unused code

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,28 +1,11 @@
 import React from "react"
 
-import ScrollTo from "@/components/ui/scrollTo"
-
 import Header from "../../components/header"
-
-const navitems = [
-  {
-    name: "services",
-    scrollTo: "services",
-  },
-]
 
 export default function LandingLayout(props: { children?: React.ReactNode }) {
   return (
     <>
-      <Header>
-        {/* {navitems.map((item, index) => {
-          return (
-            <ScrollTo key={index} elementId={item.scrollTo}>
-              {item.name}
-            </ScrollTo>
-          )
-        })} */}
-      </Header>
+      <Header />
       {props.children}
     </>
   )
--- src/components/ui/scrollTo.tsx
+++ src/components/ui/scrollTo.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react"
 
 import { Button, type ButtonProps } from "./button"
@@ -5,25 +7,26 @@ import { Button, type ButtonProps } from "./button"
 interface ScrollToProps extends ButtonProps {
   elementId: string
 }
-const ScrollTo: React.FC<ScrollToProps> = ({
-  elementId,
-  children,
-  className,
-  ...props
-}) => {
-  const handleScrollTo = () => {
-    const element = document.getElementById(elementId)
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" })
-      return
-    } else {
-      console.error(`Element with id ${elementId} not found`)
+
+const ScrollTo = React.forwardRef<HTMLButtonElement, ScrollToProps>(
+  ({ elementId, children, className, ...props }) => {
+    const handleScrollTo = () => {
+      const element = document.getElementById(elementId)
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" })
+        return
+      } else {
+        console.error(`Element with id ${elementId} not found`)
+      }
     }
+
+    return (
+      <Button {...props} onClick={handleScrollTo} className={className}>
+        {children}
+      </Button>
+    )
   }
-  return (
-    <Button {...props} onClick={handleScrollTo} className={className}>
-      {children}
-    </Button>
-  )
-}
+)
+ScrollTo.displayName = "ScrollTo"
+
 export default ScrollTo
