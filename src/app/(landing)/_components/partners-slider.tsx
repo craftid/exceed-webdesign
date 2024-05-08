f4cb545c308801993be0217bdf1fa64158fa76f2
Abdul Rafay Shaikh <abdul.rafay@mayabytes.com>
2024-05-09 04:23:16 +0500

    Update partners slider and global CSS styles

--- src/app/(landing)/_components/partners-slider.tsx
+++ src/app/(landing)/_components/partners-slider.tsx
@@ -3,7 +3,7 @@ import Marquee from "@/components/common/marquee"
 export function PartnerSlider() {
   return (
     <Marquee baseVelocity={1}>
-      <h2 className="text-shadow-custom bg-transparent text-center text-lg font-black uppercase text-gray-200 md:text-xl lg:text-3xl xl:text-4xl 2xl:text-[58px]">
+      <h2 className="text-shadow-custom bg-transparent text-center text-lg font-black uppercase text-background shadow  md:text-xl lg:text-3xl xl:text-4xl 2xl:text-[58px] 2xl:leading-[72px]">
         Are You a Musician, Creator, or a Professional Athlete?
       </h2>
     </Marquee>
